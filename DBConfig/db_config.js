6afc433fd2780fec123339f3f37a2899691ca90b
ayush1014 <S534807@nwmissouri.edu>
2025-01-30 13:50:48 -0600

    initial commit of node express server boiler plate

---
+++ DBConfig/db_config.js
@@ -0,0 +1,14 @@
+const {Sequelize} = require('sequelize');
+require('dotenv').config();
+
+const database = new Sequelize({
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    port: process.env.DB_PORT
+});
+
+module.exports = database;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const {Sequelize} = require('sequelize');
+const db = require('./DBConfig/db_config');
+const routes = require('./Routes/routes')
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use('/nwmsu/fleet_management', routes);
+
+app.get('/',(req,res)=>{
+    res.json({
+        message: 'NWMSU Fleet Management Backend Server is running well and good; All the api\'s are working prefectly and smooth'
+    })
+});
+
+db.sync().then(()=>{
+        console.log('database is synced and running well');
+});
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8000;
+server.listen(PORT,()=>{
+        console.log(`Server is running well on PORT ${PORT}`)
+    }
+);
+
+module.exports = (req, res) => {
+    app(req,res);
+}
